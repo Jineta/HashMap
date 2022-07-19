c136b7d12d442bdcbe377a487b7d2bc3dd7f05d9
jineta <jineta@ukr.net>
2022-07-19 20:41:17 +0300

    0

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -1,2 +1,13 @@
-public interface MyHashMap {
+import java.util.Collection;
+
+public interface MyHashMap<K, V> {
+    V put(K key, V value);
+
+    V get(K key);
+
+    V remove(K key);
+
+    boolean remove(K key, V value);
+
+    int size();
 }
